527bc2cc15fc601a5fb7b4c200566d293ff0bc38
Michaelangelo Jong <mike96jong@gmail.com>
2013-06-24 23:39:57 -0300

    update REAME and add Polygon_Object.js

---
+++ Polygon_Object.js
@@ -0,0 +1,230 @@
+/*******NOTES*******
+ * 
+ * Created by: Michaelangelo Jong
+ * 
+ * look at the README file for more information
+ * 
+*******End of notes*******/
+
+/*******Polygon Object*******/
+function Polygon(xPoints,yPoints){
+	var x=[];
+	var y=[];
+	if(xPoints.constructor == Array && yPoints.constructor == Array){
+		x=xPoints;
+		y=yPoints;
+	}
+	this.set=set;
+	this.set();
+//Attributes
+	function set(){
+		var i=0;
+		var wi=x[i];
+		var hi=y[i];
+		while(i<x.length){
+			if(wi>x[i]){
+				wi=x[i];
+			}
+			if(hi>y[i]){
+				hi=y[i];
+			}
+			i++;
+		}
+		i=0;
+		var wf=x[i];
+		var hf=y[i];
+		while(i<x.length){
+			if(wf<x[i]){
+				wf=x[i];
+			}
+			if(hf<y[i]){
+				hf=y[i];
+			}
+			i++;
+		}
+		this.xPoints=x;
+		this.yPoints=y;
+		this.xCenter=wi+(wf-wi)/2;
+		this.yCenter=hi+(hf-hi)/2;
+		this.height=hf-hi;
+		this.width=wf-wi;
+		this.xCoor=wi;
+		this.yCoor=hi;
+		this.sides=x.length;
+	}	
+//Methods
+	this.addPoint=addPoint;
+	this.removePoint=removePoint;
+	this.movePoint=movePoint;
+	this.reset=reset;
+	this.translate=translate;
+	this.resize=resize;
+	this.contains=contains;
+//Method Actions
+	function addPoint(xPoint, yPoint, pointIndex){
+		if(typeof pointIndex === 'number'){
+			pointIndex=pointIndex;
+		}else{
+			pointIndex=x.length;
+		}
+		if(typeof xPoint === 'number' && typeof yPoint === 'number'){
+			x.splice(pointIndex,0,xPoint);
+			y.splice(pointIndex,0,yPoint);
+		}
+		this.set();
+	}
+	function removePoint(pointIndex){
+		if(typeof pointIndex === 'number'){
+			x.splice(pointIndex,1);
+			y.splice(pointIndex,1);
+		}else{
+			x.splice(x.length-1,1);
+			y.splice(y.length-1,1);
+		}
+		this.set();
+	}
+	function movePoint(xPoint, yPoint, pointIndex){
+		if(typeof pointIndex === 'number'){
+			pointIndex=pointIndex;
+		}else{
+			pointIndex=x.length-1;
+		}
+		if(typeof xPoint === 'number' && typeof yPoint === 'number'){
+			x.splice(pointIndex,1,xPoint);
+			y.splice(pointIndex,1,yPoint);
+		}
+		this.set();
+	}
+	function reset(xPoints, yPoints){
+		x.splice(0,x.length);
+		y.splice(0,y.length);
+		if(xPoints.constructor == Array && yPoints.constructor == Array){
+			x=xPoints;
+			y=yPoints;
+		}
+		this.set();
+	}
+	function translate(xMove, yMove){
+		if(typeof xMove === 'number' && typeof yMove === 'number'){
+			var i=0;
+			while(i<x.length){
+				x[i]+=xMove;
+				y[i]+=yMove;
+				i++;
+			}
+		}
+		this.set();
+	}
+	function resize(sizeMultiplier){
+		if(typeof sizeMultiplier === 'number'){
+			var xTemp=this.xCoor;
+			var yTemp=this.yCoor;
+			var i=0;
+			while(i<x.length){
+				x[i]*=sizeMultiplier;
+				y[i]*=sizeMultiplier;
+				i++;
+			}
+		}
+		this.set();
+		this.translate(-1*(this.xCoor-xTemp),-1*(this.yCoor-yTemp));
+	}
+	function contains(xPoint, yPoint){
+		var pathsCrossed=0;
+		var V=0;
+		if(typeof xPoint === 'number' && typeof yPoint === 'number'){
+			if(xPoint >= this.xCoor && this.xCoor+this.width >= xPoint  &&  yPoint >= this.yCoor && this.yCoor+this.height >= yPoint){
+				x.push(x[0]);
+				y.push(y[0]);
+				var i=0;
+				while(i<x.length-1){
+					if(x[i] < xPoint && xPoint < x[i+1]){
+						V=(y[i+1]-y[i])/(x[i+1]-x[i])*(xPoint-x[i])+y[i];
+						if(yPoint  >= V){
+							pathsCrossed++;
+						}
+					}else if(x[i+1] < xPoint && xPoint < x[i]){
+						V=(y[i]-y[i+1])/(x[i]-x[i+1])*(xPoint-x[i])+y[i];
+						if(yPoint >= V){
+							pathsCrossed++;
+						}
+					}else if(xPoint==x[i]){
+						if(yPoint==y[i]){return true;}
+						if((x[i-1]<=x[i]&&x[i]<=x[i+1])||(x[i-1]>=x[i]&&x[i]>=x[i+1])){
+							if(yPoint >= y[i]){
+								pathsCrossed++;
+							}
+						}
+					}
+					i++;
+				}
+				x.pop();
+				y.pop();
+				if(pathsCrossed%2 == 1){
+					return true;
+				}else{
+					return false;
+				}
+			}else{
+				return false;
+			}
+		}else{
+			return false;
+		}
+	}
+}
+/*******Canvas methods to draw polygons*******/
+CanvasRenderingContext2D.prototype.strokePolygon=function(polygon){
+	var x=[];
+	var y=[];
+	if(polygon.constructor == Polygon){
+		x=polygon.xPoints;
+		y=polygon.yPoints;
+	}
+	var i=0;
+	this.beginPath();
+	this.moveTo(x[i],y[i]);
+	while(i<x.length){
+		i++;
+		this.lineTo(x[i],y[i]);
+	}
+	this.closePath();
+	this.stroke();
+}
+CanvasRenderingContext2D.prototype.fillPolygon=function(polygon){
+	var x=[];
+	var y=[];
+	if(polygon.constructor == Polygon){
+		x=polygon.xPoints;
+		y=polygon.yPoints;
+	}
+	var i=0;
+	this.beginPath();
+	this.moveTo(x[i],y[i]);
+	while(i<x.length){
+		i++;
+		this.lineTo(x[i],y[i]);
+	}
+	this.closePath();
+	this.fill();
+}
+CanvasRenderingContext2D.prototype.clearPolygon=function(polygon){
+	var x=[];
+	var y=[];
+	if(polygon.constructor == Polygon){
+		x=polygon.xPoints;
+		y=polygon.yPoints;
+	}
+	var i=0;
+	this.globalCompositeOperation = "destination-out";
+	this.beginPath();
+	this.moveTo(x[i],y[i]);
+	while(i<x.length){
+		i++;
+		this.lineTo(x[i],y[i]);
+	}
+	this.closePath();
+	this.fill();
+	this.globalCompositeOperation = "source-over";
+}
+/*******End of Code*******/
--- README.md
+++ README.md
@@ -1,4 +1,125 @@
 Polygon-Object-JavaScript
 =========================
 
-Easy to use Polygon Object for JavaScript and HTML 5
+Make a new Polygon object:
+
+	var polygon = new Polygon(xPoints,yPoints)
+		where:
+			xPoints is an Array of x coordinates
+			yPoints is an Array of y coordinates
+		if no arguments:
+			an empty polygon will be created
+			
+Polygon Attributes:
+
+	Polygon.xPoints
+		returns an array of x points
+		
+	Polygon.yPoints
+		returns an array of y points
+		
+	Polygon.xCenter
+		returns x coordinate of the centre point of the polygon
+		
+	Polygon.yCenter
+		returns y coordinate of the centre point of the polygon
+		
+	Polygon.height
+		returns the height of the polygon
+		
+	Polygon.width
+		returns the width of the polygon
+		
+	Polygon.xCoor
+		returns the lowest x coordinate of the polygon
+		
+	Polygon.yCoor
+		returns the lowest y coordinate of the polygon
+		
+	Polygon.sides
+		returns the number of sides in the polygon
+
+Polygon Methods:
+
+	Polygon.addPoint(xPoint, yPoint, pointIndex)	
+		where:
+			xPoint is the x coordinates of the point
+			yPoint is the y coordinates of the point
+			pointIndex is where the point will be added
+		does:
+			adds a point to the polygon
+		if no argument(pointIndex):
+			will add a point on the end
+			
+	Polygon.removePoint(pointIndex)
+		where:
+			pointIndex is which point will be removed
+		does:
+			removes a point from the polygon
+		if no argument(pointIndex):
+			will remove the last point
+			
+	Polygon.movePoint(xPoint, yPoint, pointIndex)
+		where:
+			xPoint is the x coordinates of the point
+			yPoint is the y coordinates of the point
+			pointIndex is which point will be moved
+		does:
+			moves a point in the polygon
+		if no argument(pointIndex):
+			will move the last point
+			
+	Polygon.reset(xPoints, yPoints)
+		where:
+			xPoints is an Array of x coordinates
+			yPoints is an Array of y coordinates
+		does:
+			resets all points in polygon
+		if no arguments:
+			the polygon will be emptyed
+			
+	Polygon.translate(xMove, yMove)
+		where:
+			xMove is the amount to move in the x-direction
+			yMove is the amount to move in the y-direction
+		does:
+			translate the polygon
+			
+	Polygon.resize(sizeMultiplier)
+		where:
+			sizeMultiplier is the scale in which to resize
+		does:
+			resizes the polygon
+			
+	Polygon.contains(xPoints, yPoints)
+		where:
+			xPoint is the x coordinates of the point to be tested
+			yPoint is the y coordinates of the point to be tested
+		does:
+			returns true if the point is inside of the polygon
+			returns false if the point is outside of the polygon
+
+CanvasRenderingContext2D Methods for drawing polygons:
+
+	CanvasRenderingContext2D.strokePolygon(polygon)
+		where:
+			polygon is a Polygon object
+		does:
+			draws the outline of the polygon
+			
+	CanvasRenderingContext2D.fillPolygon(polygon)
+		where:
+			polygon is a Polygon object
+		does:
+			draws a filled polygon
+			
+	CanvasRenderingContext2D.clearPolygon(polygon)
+		where:
+			polygon is a Polygon object
+		does:
+			clears the area contained in the polygon
+
+Created By:
+	
+	Michaelangelo Jong
+	
